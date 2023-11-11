0bba5eedb5f72e9da372afcf784a4200bab4dea5
Ben Iofel <ben@iofel.me>
2023-11-11 01:20:10 -0500

    start parsing

--- doc.go
+++ doc.go
@@ -5,8 +5,48 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+type manPage struct {
+	name    string
+	section int
+	date    string
+}
+
+func parseManDoc(doc string) manPage {
+	title, _ := regexp.Compile(`\.Dt ([A-Z]+) (\d+)`)
+
+	page := manPage{}
+	for _, line := range strings.Split(doc, "\n") {
+		switch {
+
+		case strings.HasPrefix(line, ".\\\""):
+			fmt.Printf("// %s\n", line)
+
+		case strings.HasPrefix(line, ".Dd "):
+			page.date = line[4:]
+
+		case title.MatchString(line):
+			parts := title.FindStringSubmatch(line)
+			fmt.Println(parts)
+			page.name = parts[1]
+			section, err := strconv.Atoi(parts[2])
+			if err != nil {
+				panic(err)
+			}
+			page.section = section
+
+		default:
+			fmt.Printf("?? %s\n", line)
+
+		}
+	}
+	fmt.Printf("%v\n", page)
+	return page
+}
 
 func findDoc(target string) string {
 	var foundPath string
@@ -34,5 +74,16 @@ func main() {
 	}
 
 	target := os.Args[1]
-	fmt.Println(findDoc(target))
+	manFile := findDoc(target)
+	if manFile == "" {
+		fmt.Fprintf(os.Stderr, "cannot find man page for \"%s\"\n", target)
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(manFile)
+	if err != nil {
+		panic(err)
+	}
+
+	parseManDoc(string(data))
 }
